18b62025118450310f381a4135235d4ac434bd50
Hristijan <lubeskih@gmail.com>
2020-04-06 20:44:15 +0200

    Потсредена скрипта и README (#1)
    ## Description
    
    Пул-риквестов ја реименува `sentenceConventer.py` во `extractor.py` и го менува поголемиот (ако не и целиот) дел од текот и логиката на програмата. Исто, го потсредува и `README` фајлот.
    
    ## Commits
    * add extractor script, rename and improve README
    
    * fix typo in config.py
    
    * move config to extractor, remove config.py; improve docstring
    
    * fixes the bug with not appending valid sentences
    
    * add MK specific characters to whitelist
    
    * whitelist the '—' character
    
    * whitelist another commonly used character
    
    * whitelist the semicolon character
    
    * Update README.md
    
    Корекција на „контрибуираш“ со „придонесеш“.
    
    Co-Authored-By: Matej Plavevski <matej.plavevski+github@gmail.com>
    
    * Fixes the bug with capitalization.
    
    Co-Authored-By: Matej Plavevski <matej.plavevski+github@gmail.com>
    
    * fix the syntaxwarning, remove unneeded 'else'
    
    Co-authored-by: Matej Plavevski <matej.plavevski+github@gmail.com>

---
+++ README.md
@@ -0,0 +1,63 @@
+# Собирач на реченици
+Алатка која би требало да ја олесни работата во собирање на реченици за [проектот](https://mozilla.mk/2020/04/02/%d0%b0%d0%ba%d1%86%d0%b8%d1%98%d0%b0-common-voice-%d0%bd%d0%b0-%d0%bc%d0%b0%d0%ba%d0%b5%d0%b4%d0%be%d0%bd%d1%81%d0%ba%d0%b8/): [Mozilla Common Voice](https://voice.mozilla.org/mk). Алатката е далеку од совршена и подлежи на промени. Не се срами да придонесеш во нејзино подобрување и оптимизирање. :)
+
+### Потребни алатки
+- `wget`
+- `python3`
+- Опционално: `virtualenv`
+
+### Инсталација
+
+```
+$ git clone https://github.com/skopjehacklab/sentence-collector.git && cd sentence-collector
+```
+
+##### Ако користиш `virtualenv`:
+
+```
+$ virtualenv venv --python=python3
+$ source venv/bin/activate
+```
+
+И потоа во истата датотека:
+
+```
+pip install -r requirements.txt
+```
+
+### Упатство за користење
+
+1. Преземете веб-страница чија содржина е објавена со лиценцата [CC-0](https://creativecommons.org/share-your-work/public-domain/cc0/), со помош на следнава команда:
+
+```
+wget \
+     --recursive \
+     --no-clobber \
+     --page-requisites \
+     --html-extension \
+     --convert-links \
+     --reject '*.js,*.css,*.ico,*.txt,*.gif,*.jpg,*.jpeg,*.png,*.mp3,*.pdf,*.tgz,*.flv,*.avi,*.mpeg,*.iso' \
+     --accept html \
+     --restrict-file-names=windows \
+     --domains [ДОМЕЈНОТ НА СТРАНАТА ТУКА] \
+     --no-parent \
+         [ВЕБ САЈТОТ ТУКА]
+```
+
+Оставете командата да работи додека ги презема сите страници кои имаат некаква текст содржина.
+
+2. Во `raw_scraper.py`, промени ја променливата `searchFolder` со датотеката каде што се наоѓаат преземаните содржини.
+
+### Што ако веќе имам текст и само сакам да извадам реченици?
+
+Изврши ја следнава команда:
+
+```
+$ cat Hamlet.txt | python extractor.py
+```
+
+Програмата ќе ви каже колку реченици се успешно извадени и во кој фајл се зачувани. Фајлот ќе се креира во истата датотека кај што е програмата.
+
+### Лиценца
+
+Лиценцата можеш да ја најдеш [овде](LICENSE).
---
+++ extractor.py
@@ -0,0 +1,137 @@
+from time import time
+import fileinput
+import re
+
+# Packed config of variables used in the program
+config = {
+	"MAX_WORDS": 14,
+	"OUTPUT_FILE": f"{int(time() * 1000)}-output.txt",
+	"WHITELISTED_CHARACTERS": list("абвгдѓеѐжзѕиѝјклљмнњопрстќуфхцчџшАБВГДЃЕЀЖЗЅИЍЈКЛЉМНЊОПРСТЌУФХЦЧЏШ;„“.?!(),—-: "),
+	"SPLIT_INTO_SENTENCES": "(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s",
+	"COMMON_MISTAKES": {
+		"Сеуште": "Сѐ уште",
+		"сеуште": "сѐ уште",
+		"Незнам": "Не знам",
+		"незнам": "не знам",
+		"Одприлика": "Отприлика",
+		"одприлика": "отприлика",
+		"Сметат": "Сметаат",
+		"сметат": "сметаат",
+	}
+}
+
+def check_character(sentence) -> bool:
+	""" Given a sentence, returns True if it finds an invalid character,
+	False otherwise
+
+	Parameters:
+		sentence (str): A sentence
+
+	Returns:
+		True (bool): If there is a not white-listed character in the sentence
+		False (bool): If all characters are valid and white-listed   
+	"""
+	for character in sentence:
+		if character not in config["WHITELISTED_CHARACTERS"]:
+			return True
+	return False
+
+
+def fix_common_mistakes(sentence) -> str:
+	""" Loops through the words of a given sentence and replaces all words that 
+	are part of the config['COMMON_MISTAKES'].
+
+	Parameters:
+		sentence (str): A sentence
+
+	Returns:
+		sentence (str): The same, or fixed sentence   
+	"""
+	for index, word in enumerate(sentence.split(" ")):
+		if index == 0 and word.islower():
+			sentence = sentence.replace(word, word.capitalize())
+		
+		if word in config["COMMON_MISTAKES"]:
+			sentence = sentence.replace(word, config["COMMON_MISTAKES"][word])
+
+	return sentence
+
+def analyze(sentences) -> list:
+	""" Analyzes given sentences and returns only the valid ones.
+
+	Parameters:
+		sentences (list[str]): List of sentences
+
+	Returns:
+		valid_sentences (list[str]): List of valid sentences   
+	"""
+	valid_sentences = []
+
+	for sentence in sentences:
+		# Empty field?
+		if not sentence:
+			continue
+
+		# Any invalid characters?
+		if check_character(sentence):
+			continue
+		
+		# Any digits?
+		if bool(re.search('[\d-]', sentence)):
+			continue
+
+		# More than 14 words?
+		if len(sentence.split(' ')) > config["MAX_WORDS"]:
+			continue
+
+		# If all of these passed, finally check for common mistakes
+		sentence = fix_common_mistakes(sentence)
+
+		valid_sentences.append(sentence)
+	
+	return valid_sentences
+
+
+def write_to_file(valid_sentences) -> None:
+	""" Writes all valid sentences into a file.
+
+	Parameters:
+		valid_sentences (list[str]): List of all valid sentences
+
+	Returns:
+		None   
+	"""
+	with open(config["OUTPUT_FILE"], 'a') as f:
+		for sentence in valid_sentences:
+			f.write(f"{sentence}\n")
+	
+	return None
+
+def main() -> None:
+	""" Program main. Loops through the received lines of text from
+	stdin, and calls the needed functions in order to extract 
+	valid sentences. Outputs information about the sentences and
+	where they extracted.
+
+	Parameters:
+		None
+
+	Returns:
+		None 
+	"""
+	all_time_valid_sentences = 0
+
+	received_lines = fileinput.input()
+	
+	for index, line in enumerate(received_lines):
+		sentences = re.split(config["SPLIT_INTO_SENTENCES"], line)
+		valid_sentences = analyze(sentences)
+
+		all_time_valid_sentences += len(valid_sentences)
+
+		write_to_file(valid_sentences)
+
+	print(f"Found {all_time_valid_sentences} valid sentences. All of them were extracted to {config['OUTPUT_FILE']}")
+	return None
+
+main()
