a638b7bf9d9cdfdd9d2402be9aea120ae0f92b53
matejmecka <matej.plavevski+github@gmail.com>
2020-04-09 19:54:44 +0200

    Move from Test folder to root directory

--- test_extractor.py
+++ test_extractor.py
@@ -1,7 +1,3 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
 from extractor import check_character, fix_common_mistakes, analyze
 
 def test_check_character():
